fb68897ce33a1fe1ca5ed318b054620371f39aae
Victor Olumide <oyewumivictor5@gmail.com>
2025-09-24 13:11:47 +0100

    feat: update app favicons

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -22,7 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <meta name="apple-mobile-web-app-title" content="VO" />
+        <meta name="apple-mobile-web-app-title" content="VO Portfolio" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
